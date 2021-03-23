f4e9a241cdfbeb6c0e7a16131f8e624bf3fcc1f5
Hiro <hiroyuki1126@gmail.com>
2021-03-23 16:00:33 +0900

    関数名、引数名を変更。

--- views/common/header.php
+++ views/common/header.php
@@ -23,13 +23,13 @@ foreach ($breadcrumb_list as $breadcrumb => $url) {
 <h1><?= $contents_title ?></h1>
 <ul>
   <li>公開日：
-    <time itemprop="datePublished" datetime="<?= get_datetime_iso8601($pub_date) ?>">
-      <?= get_date_japanese($pub_date), PHP_EOL ?>
+    <time itemprop="datePublished" datetime="<?= get_time_iso8601($ptime) ?>">
+      <?= get_date_jp($ptime), PHP_EOL ?>
     </time>
   </li>
   <li>更新日：
-    <time itemprop="dateModified" datetime="<?= get_datetime_iso8601($mod_date) ?>">
-      <?= get_date_japanese($mod_date), PHP_EOL ?>
+    <time itemprop="dateModified" datetime="<?= get_time_iso8601($mtime) ?>">
+      <?= get_date_jp($mtime), PHP_EOL ?>
     </time>
   </li>
 </ul>
