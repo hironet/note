0d1c2fd350c1f3e70d15d20705a186e063cdbd97
Hiro <hiroyuki1126@gmail.com>
2021-03-27 15:03:22 +0900

    全体的に処理内容を修正。

--- controllers/routing.php
+++ controllers/routing.php
@@ -1,26 +1,50 @@
 <?php
+function article_list($category) {
+  global $contents_title;
+  global $contents_path;
+  global $meta_description;
+  global $meta_keywords;
+  global $breadcrumb_list;
+
+  $contents_title = $category;
+  $contents_path = '/contents/list.php';
+  $meta_description = "このページには、{$category}に関する情報をまとめています。";
+  $meta_keywords = $category;
+  $breadcrumb_list = [$category => ''];
+}
+
 function not_found() {
   global $contents_title;
   global $contents_path;
+
   $contents_title = 'Not Found';
   $contents_path = '/contents/404.php';
 }
 
 $category= isset($_GET['c']) ? $_GET['c'] : 'top';
 $page= isset($_GET['p']) ? $_GET['p'] : 'list';
+
 switch ($category) {
   case 'top':
     $contents_title = '';
     $contents_path = '/contents/top.php';
     break;
   case 'windows':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Windowsに関する情報をまとめています。';
-        $meta_keywords = 'windows';
-        $contents_title = 'Windows';
-        $contents_path = '/contents/windows/list.php';
-        $breadcrumb_list = ['Windows' => ''];
+        article_list('Windows');
         break;
       default:
         not_found();
@@ -28,13 +52,21 @@ switch ($category) {
     }
     break;
   case 'macos':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、macOSに関する情報をまとめています。';
-        $meta_keywords = 'macos,mac';
-        $contents_title = 'macOS';
-        $contents_path = '/contents/macos/list.php';
-        $breadcrumb_list = ['macOS' => ''];
+        article_list('macOS');
         break;
       default:
         not_found();
@@ -42,51 +74,43 @@ switch ($category) {
     }
     break;
   case 'linux':
+    $titles = [
+      'command' => 'Linuxコマンドの便利な使い方',
+      'centos7-setting' => 'CentOS 7.x 設定方法',
+    ];
+
     $descriptions = [
       'command' => '知っておくと色んな場面で便利になるコマンドの使い方を紹介しています。',
       'centos7-setting' => 'CentOS 7.xを使用するにあたって、よく行う設定を紹介しています。',
     ];
-    $descriptions = array_merge($descriptions, array_values($descriptions));
 
     $keywords = [
       'command' => 'linux,command',
       'centos7-setting' => 'linux,centos,設定',
     ];
-    $keywords = array_merge($keywords, array_values($keywords));
-
-    $titles = [
-      'command' => 'Linuxコマンドの便利な使い方',
-      'centos7-setting' => 'CentOS 7.x 設定方法',
-    ];
-    $titles = array_merge($titles, array_values($titles));
 
     $links = [
       'command' => '.?c=linux&p=command',
       'centos7-setting' => '.?c=linux&p=centos7-setting',
     ];
-    $links = array_merge($links, array_values($links));
-
-    $breadcrumb_list = ['Linux' => '.?c=linux'];
 
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Linuxに関する情報をまとめています。';
-        $meta_keywords = 'linux';
-        $contents_title = 'Linux';
-        $contents_path = '/contents/list.php';
-        $breadcrumb_list = ['Linux' => ''];
+        article_list('Linux');
         break;
       case 'command':
-        $meta_description = $descriptions['command'];
-        $meta_keywords = $keywords['command'];
         $contents_title = $titles['command'];
         $contents_path = '/contents/linux/command.php';
+        $meta_description = $descriptions['command'];
+        $meta_keywords = $keywords['command'];
+        $breadcrumb_list = ['Linux' => '.?c=linux'];
         break;
       case 'centos7-setting':
-        $meta_description = $descriptions['centos7-setting'];
-        $meta_keywords = $keywords['centos7-setting'];
         $contents_title = $titles['centos7-setting'];
         $contents_path = '/contents/linux/centos7-setting.php';
+        $meta_description = $descriptions['centos7-setting'];
+        $meta_keywords = $keywords['centos7-setting'];
+        $breadcrumb_list = ['Linux' => '.?c=linux'];
         break;
       default:
         not_found();
@@ -94,13 +118,21 @@ switch ($category) {
     }
     break;
   case 'docker':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Dockerに関する情報をまとめています。';
-        $meta_keywords = 'docker';
-        $contents_title = 'Docker';
-        $contents_path = '/contents/docker/list.php';
-        $breadcrumb_list = ['Docker' => ''];
+        article_list('Docker');
         break;
       default:
         not_found();
@@ -108,13 +140,21 @@ switch ($category) {
     }
     break;
   case 'nginx':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Nginxに関する情報をまとめています。';
-        $meta_keywords = 'nginx';
-        $contents_title = 'Nginx';
-        $contents_path = '/contents/nginx/list.php';
-        $breadcrumb_list = ['Nginx' => ''];
+        article_list('Nginx');
         break;
       default:
         not_found();
@@ -122,13 +162,21 @@ switch ($category) {
     }
     break;
   case 'apache':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Apacheに関する情報をまとめています。';
-        $meta_keywords = 'apache';
-        $contents_title = 'Apache';
-        $contents_path = '/contents/apache/list.php';
-        $breadcrumb_list = ['Apache' => ''];
+        article_list('Apache');
         break;
       default:
         not_found();
@@ -136,13 +184,21 @@ switch ($category) {
     }
     break;
   case 'mysql':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、MySQLに関する情報をまとめています。';
-        $meta_keywords = 'mysql';
-        $contents_title = 'MySQL';
-        $contents_path = '/contents/mysql/list.php';
-        $breadcrumb_list = ['MySQL' => ''];
+        article_list('MySQL');
         break;
       default:
         not_found();
@@ -150,13 +206,21 @@ switch ($category) {
     }
     break;
   case 'wordpress':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、WordPressに関する情報をまとめています。';
-        $meta_keywords = 'wordpress';
-        $contents_title = 'WordPress';
-        $contents_path = '/contents/wordpress/list.php';
-        $breadcrumb_list = ['WordPress' => ''];
+        article_list('WordPress');
         break;
       default:
         not_found();
@@ -164,13 +228,21 @@ switch ($category) {
     }
     break;
   case 'vim':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Vimに関する情報をまとめています。';
-        $meta_keywords = 'vim,vi';
-        $contents_title = 'Vim';
-        $contents_path = '/contents/vim/list.php';
-        $breadcrumb_list = ['Vim' => ''];
+        article_list('Vim');
         break;
       default:
         not_found();
@@ -178,13 +250,21 @@ switch ($category) {
     }
     break;
   case 'vscode':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Visual Studio Codeに関する情報をまとめています。';
-        $meta_keywords = 'visual studio code,vscode';
-        $contents_title = 'Visual Studio Code';
-        $contents_path = '/contents/vscode/list.php';
-        $breadcrumb_list = ['Visual Studio Code' => ''];
+        article_list('Visual Studio Code');
         break;
       default:
         not_found();
@@ -192,13 +272,21 @@ switch ($category) {
     }
     break;
   case 'office':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Microsoft Officeに関する情報をまとめています。';
-        $meta_keywords = 'microsoft office,ms office,office';
-        $contents_title = 'Microsoft Office';
-        $contents_path = '/contents/office/list.php';
-        $breadcrumb_list = ['Microsoft Office' => ''];
+        article_list('Microsoft Office');
         break;
       default:
         not_found();
@@ -206,13 +294,21 @@ switch ($category) {
     }
     break;
   case 'git':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Gitに関する情報をまとめています。';
-        $meta_keywords = 'git';
-        $contents_title = 'Git';
-        $contents_path = '/contents/git/list.php';
-        $breadcrumb_list = ['Git' => ''];
+        article_list('Git');
         break;
       default:
         not_found();
@@ -220,13 +316,21 @@ switch ($category) {
     }
     break;
   case 'vagrant':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Vagrantに関する情報をまとめています。';
-        $meta_keywords = 'vagrant';
-        $contents_title = 'Vagrant';
-        $contents_path = '/contents/vagrant/list.php';
-        $breadcrumb_list = ['Vagrant' => ''];
+        article_list('Vagrant');
         break;
       default:
         not_found();
@@ -234,13 +338,21 @@ switch ($category) {
     }
     break;
   case 'wireshark':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Wiresharkに関する情報をまとめています。';
-        $meta_keywords = 'wireshark';
-        $contents_title = 'Wireshark';
-        $contents_path = '/contents/wireshark/list.php';
-        $breadcrumb_list = ['Wireshark' => ''];
+        article_list('Wireshark');
         break;
       default:
         not_found();
@@ -248,13 +360,21 @@ switch ($category) {
     }
     break;
   case 'html':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、HTML/CSSに関する情報をまとめています。';
-        $meta_keywords = 'html,css';
-        $contents_title = 'HTML/CSS';
-        $contents_path = '/contents/html/list.php';
-        $breadcrumb_list = ['HTML/CSS' => ''];
+        article_list('HTML/CSS');
         break;
       default:
         not_found();
@@ -262,13 +382,21 @@ switch ($category) {
     }
     break;
   case 'javascript':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、JavaScriptに関する情報をまとめています。';
-        $meta_keywords = 'javascript,js';
-        $contents_title = 'JavaScript';
-        $contents_path = '/contents/javascript/list.php';
-        $breadcrumb_list = ['JavaScript' => ''];
+        article_list('JavaScript');
         break;
       default:
         not_found();
@@ -276,13 +404,21 @@ switch ($category) {
     }
     break;
   case 'php':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、PHPに関する情報をまとめています。';
-        $meta_keywords = 'php';
-        $contents_title = 'PHP';
-        $contents_path = '/contents/php/list.php';
-        $breadcrumb_list = ['PHP' => ''];
+        article_list('PHP');
         break;
       default:
         not_found();
@@ -290,13 +426,21 @@ switch ($category) {
     }
     break;
   case 'ruby':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Rubyに関する情報をまとめています。';
-        $meta_keywords = 'ruby';
-        $contents_title = 'Ruby';
-        $contents_path = '/contents/ruby/list.php';
-        $breadcrumb_list = ['Ruby' => ''];
+        article_list('Ruby');
         break;
       default:
         not_found();
@@ -304,13 +448,21 @@ switch ($category) {
     }
     break;
   case 'bash':
+    $titles = [
+    ];
+
+    $descriptions = [
+    ];
+
+    $keywords = [
+    ];
+
+    $links = [
+    ];
+
     switch ($page) {
       case 'list':
-        $meta_description = 'このページには、Bashに関する情報をまとめています。';
-        $meta_keywords = 'bash';
-        $contents_title = 'Bash';
-        $contents_path = '/contents/bash/list.php';
-        $breadcrumb_list = ['Bash' => ''];
+        article_list('Bash');
         break;
       default:
         not_found();
--- views/contents/list.php
+++ views/contents/list.php
@@ -2,13 +2,20 @@
 <?php include_once(__DIR__ . '/../common/header.php'); ?>
 </header>
 <div class="row">
-<?php for ($i = 0; $i < count($titles) / 2; $i++) { ?>
+<?php
+// 配列のキーを数値に変換
+$titles_2 = array_values($titles);
+$descriptions_2 = array_values($descriptions);
+$links_2 = array_values($links);
+
+for ($i = 0; $i < count($titles_2); $i++) {
+?>
   <div class="col-12 col-md-6 col-lg-4">
     <div class="card my-2">
       <div class="card-body">
-        <h5 class="card-title"><?= $titles[$i] ?></h5>
-        <p class="card-text"><?= $descriptions[$i] ?></p>
-        <a href="<?= $links[$i] ?>" class="card-link">詳しく見る</a>
+        <h5 class="card-title"><?= $titles_2[$i] ?></h5>
+        <p class="card-text"><?= $descriptions_2[$i] ?></p>
+        <a href="<?= $links_2[$i] ?>" class="card-link">詳しく見る</a>
       </div>
     </div><!-- card -->
   </div><!-- col -->
