0cadbc97d062655b309460e2734a991812824166
Hiro <hiroyuki1126@gmail.com>
2021-03-28 18:49:04 +0900

    スタイルを修正。

--- views/head.php
+++ views/head.php
@@ -5,8 +5,13 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" tegrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <title><?= empty($contents_title) ? '' : $contents_title . ' | ' ?>Hiro's tech note</title>
 <style>
+  p, ul, ol, li {
+    margin: 0;
+    padding: 0;
+  }
+
   body {
-    padding-top: 5.0rem;
+    padding-top: 5rem;
   }
 
   nav.navbar {
@@ -18,14 +23,18 @@
     background-color: #5c73b7;
     color: #ffffff;
     font-size: 1.5rem;
-    margin: 2.0rem 0.0rem 0.5rem;
+    margin: 2rem 0 0.5rem;
     opacity: 0.9;
     padding: 0.2rem 0.5rem;
   }
 
+  ul, ol {
+    padding-left: 2rem;
+  }
+
   pre.prettyprint {
     font-family: Monaco, Menlo, Consolas, 'Courier New', Courier, monospace, sans-serif;
-    margin: 0.0rem 0.0rem 2.0rem 1.0rem;
+    margin: 0.2rem 0 1rem 1rem;
     padding: 0.2rem 0.5rem;
   }
 </style>
