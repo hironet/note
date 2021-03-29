9efcb6737a2998f7c1ab80b1955ace41970326e6
Hiro <hiroyuki1126@gmail.com>
2021-03-29 21:10:36 +0900

    軽微な修正。

--- views/contents/linux/centos7-setting.php
+++ views/contents/linux/centos7-setting.php
@@ -26,33 +26,25 @@ $mtime = get_mtime(__FILE__);
 </ol>
 <h3 id="network" class="title">ネットワーク設定</h3>
 <p>ネットワークインタフェースを有効化する。
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] connection.autoconnect yes
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] connection.autoconnect yes</code></pre>
 <p>IPアドレスとサブネットマスクを設定する。</p>
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.addresses [IPアドレス]/[サブネットマスク]
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.addresses [IPアドレス]/[サブネットマスク]</code></pre>
 <p>デフォルトゲートウェイを設定する。</p>
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.gateway [デフォルトゲートウェイのIPアドレス]
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.gateway [デフォルトゲートウェイのIPアドレス]</code></pre>
 <p>スタティックルートを設定する。</p>
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.routes "[宛先ネットワークのアドレス]/[サブネットマスク] [ゲートウェイのIPアドレス]"
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.routes "[宛先ネットワークのアドレス]/[サブネットマスク] [ゲートウェイのIPアドレス]"</code></pre>
 <p>2つ目以降のスタティックルートを設定する場合は、<code>ipv4.routes</code>の前に<code>+</code>を付ける。</p>
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] +ipv4.routes "[宛先ネットワークのアドレス]/[サブネットマスク] [ゲートウェイのIPアドレス]"
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] +ipv4.routes "[宛先ネットワークのアドレス]/[サブネットマスク] [ゲートウェイのIPアドレス]"</code></pre>
 <p>DNSサーバを設定する。</p>
 <ul>
   <li>DNSサーバのIPアドレスは、カンマ区切りで複数指定できる。</li>
 </ul>
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.dns [DNSサーバのIPアドレス]
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.dns [DNSサーバのIPアドレス]</code></pre>
 <p>IPアドレスを固定割り当てに設定する。</p>
-<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.method manual
-</code></pre>
+<pre class="block"><code>$ nmcli c modify [ネットワークインタフェース名] ipv4.method manual</code></pre>
 <p>ネットワークインタフェースを再起動する。</p>
 <pre class="block"><code>$ nmcli c down [ネットワークインタフェース名]
-$ nmcli c up [ネットワークインタフェース名]
-</code></pre>
+$ nmcli c up [ネットワークインタフェース名]</code></pre>
 <h3 id="yum" class="title">パッケージの管理（Yum）</h3>
 <div class="table-responsive">
   <table class="table table-striped table-sm">
@@ -102,24 +94,20 @@ $ nmcli c up [ネットワークインタフェース名]
 <pre class="block"><code>$ mkdir /media/CentOS
 $ mount -t iso9660 /dev/cdrom /media/CentOS
 $ yum -y --disablerepo=* --enablerepo=c7-media install [パッケージ名]
-$ umount /media/CentOS
-</code></pre>
+$ umount /media/CentOS</code></pre>
 <p>yumでインストールされたファイル一覧を表示する。</p>
 <ul>
   <li>事前に、<code>yum-utils</code>パッケージをインストールしておく必要がある。</li>
 </ul>
-<pre class="block"><code>$ repoquery --list [パッケージ名]
-</code></pre>
+<pre class="block"><code>$ repoquery --list [パッケージ名]</code></pre>
 <p>RPMパッケージをダウンロードする。</p>
 <ul>
   <li>事前に、<code>yum-utils</code>パッケージをインストールしておく必要がある。</li>
 </ul>
-<pre class="block"><code>$ yumdownloader --resolve [パッケージ名]
-</code></pre>
+<pre class="block"><code>$ yumdownloader --resolve [パッケージ名]</code></pre>
 <h3 id="rpm" class="title">パッケージの管理（RPM）</h3>
 <p>ファイルがどのパッケージに含まれるかを確認する。</p>
-<pre class="block"><code>$ rpm -qf [ファイルパス]
-</code></pre>
+<pre class="block"><code>$ rpm -qf [ファイルパス]</code></pre>
 <h3 id="service" class="title">サービスの操作</h3>
 <div class="table-responsive">
   <table class="table table-striped table-sm">
@@ -163,24 +151,19 @@ $ umount /media/CentOS
 </div><!-- table-responsive -->
 <h3 id="other" class="title">その他</h3>
 <p>システムの時刻・日付設定を確認する。</p>
-<pre class="block"><code>$ timedatectl status
-</code></pre>
+<pre class="block"><code>$ timedatectl status</code></pre>
 <p>タイムゾーンを<code>Asia/Tokyo (JST)</code>に設定する。</p>
-<pre class="block"><code>$ timedatectl set-timezone Asia/Tokyo
-</code></pre>
+<pre class="block"><code>$ timedatectl set-timezone Asia/Tokyo</code></pre>
 <p>OS起動時にコマンドを自動実行する。</p>
 <pre class="block"><code>$ cat /etc/rc.d/rc.local
 [実行したいコマンド]
-$ chmod +x /etc/rc.d/rc.local
-</code></pre>
+$ chmod +x /etc/rc.d/rc.local</code></pre>
 <p>OSログイン時にメッセージを表示する。</p>
 <pre class="block"><code>$ cat /etc/motd
-[表示したいメッセージ]
-</code></pre>
+[表示したいメッセージ]</code></pre>
 <p>SSH鍵ペアを作成し、SSH公開鍵を接続先ホストに登録する。</p>
 <pre class="block"><code>$ ssh-keygen -t rsa
-$ ssh-copy-id -i ~/.ssh/id_rsa.pub [ユーザ名]@[接続先ホスト]
-</code></pre>
+$ ssh-copy-id -i ~/.ssh/id_rsa.pub [ユーザ名]@[接続先ホスト]</code></pre>
 <p>SSH接続設定を保存しておき、接続コマンドを簡単にする。</p>
 <pre class="block"><code>$ cat ~/.ssh/config
 Host [接続名]
@@ -188,8 +171,7 @@ HostName [ホスト名]
 User [ユーザ名]
 IdentityFile [SSH秘密鍵のパス]
 $ chmod 600 ~/.ssh/config
-$ ssh [接続名]
-</code></pre>
+$ ssh [接続名]</code></pre>
 <p>HTTPプロキシを設定する。</p>
 <pre class="block"><code>$ cat /etc/profile.d/proxy.sh
 export http_proxy=[プロキシのホスト名またはIPアドレス]:[ポート番号]
@@ -197,16 +179,13 @@ export HTTP_PROXY=$http_proxy
 export https_proxy=$http_proxy
 export HTTPS_PROXY=$http_proxy
 export no_proxy=127.0.0.1,localhost
-export NO_PROXY=$no_proxy
-</code></pre>
+export NO_PROXY=$no_proxy</code></pre>
 <p>cronで実行したコマンドの標準出力および標準エラー出力がメールで飛ばないようにする。</p>
 <pre class="block"><code>$ crontab -l
-[分] [時] [日] [月] [曜日] [コマンド] &gt; /dev/null 2&gt;&amp;1
-</code></pre>
+[分] [時] [日] [月] [曜日] [コマンド] &gt; /dev/null 2&gt;&amp;1</code></pre>
 <p>フォーマット付きのdateコマンドをcronで実行する。</p>
 <ul>
   <li>フォーマット中の<code>%</code>は、<code>\</code>でエスケープする必要がある点に注意。</li>
 </ul>
 <pre class="block"><code>$ crontab -l
-[分] [時] [日] [月] [曜日] [コマンド] &gt; /tmp/output-$(date '+\%Y\%m\%d').log
-</code></pre>
+[分] [時] [日] [月] [曜日] [コマンド] &gt; /tmp/output-$(date '+\%Y\%m\%d').log</code></pre>
