307c7f530297c9ada746b10852a1f172f12fc0f9
Hiro <hiroyuki1126@gmail.com>
2021-04-03 16:15:02 +0900

    新規作成。

---
+++ views/contents/wordpress/performance.php
@@ -0,0 +1,21 @@
+<?php
+$ptime = '2021-04-03 00:00:00';
+$mtime = get_mtime(__FILE__);
+?>
+<header>
+<?php include_once(__DIR__ . '/../common/header.php'); ?>
+<?php include_once(__DIR__ . '/../common/date.php'); ?>
+</header>
+<h2 class="title">リビジョン削除</h2>
+<p>データベース（MySQL）に接続し、以下SQLを実行する。</p>
+<pre class="block"><code>USE wordpress;
+SELECT COUNT(*) FROM wp_posts WHERE post_type='revision';
+DELETE FROM wp_posts WHERE post_type='revision';
+SELECT COUNT(*) FROM wp_posts WHERE post_type='revision';</code></pre>
+<h2 class="title">wp-postsテーブルのフラグメント化解消</h2>
+<p>データベース（MySQL）に接続し、リビジョンを削除した後、以下SQLを実行する。</p>
+<pre class="block"><code>USE wordpress;
+SELECT @@sql_mode;
+SET sql_mode = '';
+SELECT @@sql_mode;
+OPTIMIZE TABLE wp_posts;</code></pre>
