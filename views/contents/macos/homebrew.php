e55a948a6b7566d6e6b7ff158dffbbd864ec9258
Hiro <hiroyuki1126@gmail.com>
2021-04-03 13:49:01 +0900

    Homebrew-Caskを削除。コマンドを修正。

--- views/contents/macos/homebrew.php
+++ views/contents/macos/homebrew.php
@@ -14,12 +14,6 @@ $mtime = get_mtime(__FILE__);
   <li>
     <a href="#homebrew-command">Homebrewのコマンド</a>
   </li>
-  <li>
-    <a href="#homebrew-cask-install">Homebrew-Caskのインストール</a>
-  </li>
-  <li>
-    <a href="#homebrew-cask-command">Homebrew-Caskのコマンド</a>
-  </li>
 </ul>
 <h2 id="homebrew-install" class="title">Homebrewのインストール</h3>
 <p><a rel="noopener noreferrer" href="https://brew.sh/index_ja.html" target="_blank">Homebrew</a>の説明に従ってインストールする。</p>
@@ -49,6 +43,10 @@ $mtime = get_mtime(__FILE__);
       <td><pre class="inline"><code>$ brew install [Formula]</code></pre></td>
     </tr>
     <tr>
+      <td>GUIパッケージをインストールする。</td>
+      <td><pre class="inline"><code>$ brew install [Formula] --cask</code></pre></td>
+    </tr>
+    <tr>
       <td>パッケージをアンインストールする。</td>
       <td><pre class="inline"><code>$ brew uninstall [Formula]</code></pre></td>
     </tr>
@@ -57,8 +55,20 @@ $mtime = get_mtime(__FILE__);
       <td><pre class="inline"><code>$ brew update</code></pre></td>
     </tr>
     <tr>
+      <td>バージョンが古くなったパッケージ一覧を表示する。</td>
+      <td><pre class="inline"><code>$ brew outdated</code></pre></td>
+    </tr>
+    <tr>
+      <td>バージョンが古くなったGUIパッケージ一覧を表示する。</td>
+      <td><pre class="inline"><code>$ brew outdated --cask --greedy</code></pre></td>
+    </tr>
+    <tr>
       <td>インストールされている全てのパッケージを更新する。</td>
       <td><pre class="inline"><code>$ brew upgrade</code></pre></td>
+    </tr>
+    <tr>
+      <td>インストールされている全てのGUIパッケージを更新する。</td>
+      <td><pre class="inline"><code>$ brew upgrade --cask --greedy</code></pre></td>
     </tr>
     <tr>
       <td>インストールされている個別のパッケージを更新する。</td>
@@ -67,48 +77,6 @@ $mtime = get_mtime(__FILE__);
     <tr>
       <td>Homebrewの問題を診断する。</td>
       <td><pre class="inline"><code>$ brew doctor</code></pre></td>
-    </tr>
-  </tbody>
-</table>
-<h2 id="homebrew-cask-install" class="title">Homebrew-Caskのインストール</h3>
-<pre class="block"><code>$ brew tap caskroom/cask
-$ brew tap caskroom/versions</code></pre>
-<h2 id="homebrew-cask-command" class="title">Homebrew-Caskのコマンド</h3>
-<table class="table table-striped table-sm">
-  <thead>
-    <tr>
-      <th>操作</th>
-      <th>コマンド</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>インストールされているパッケージ一覧を表示する。</td>
-      <td><pre class="inline"><code>$ brew cask list</code></pre></td>
-    </tr>
-    <tr>
-      <td>パッケージをインストールする。</td>
-      <td><pre class="inline"><code>$ brew cask install [パッケージ名]</code></pre></td>
-    </tr>
-    <tr>
-      <td>パッケージをアンインストールする。</td>
-      <td><pre class="inline"><code>$ brew cask uninstall [パッケージ名]</code></pre></td>
-    </tr>
-    <tr>
-      <td>不要になったダウンロード済みファイルを削除する。</td>
-      <td><pre class="inline"><code>$ brew cask cleanup</code></pre></td>
-    </tr>
-    <tr>
-      <td>バージョンが古くなったパッケージ一覧を表示する。</td>
-      <td><pre class="inline"><code>$ brew cask outdated</code></pre></td>
-    </tr>
-    <tr>
-      <td>バージョンが古くなった全てのパッケージを更新する。</td>
-      <td><pre class="inline"><code>$ brew cask upgrade</code></pre></td>
-    </tr>
-    <tr>
-      <td>Homebrew-Caskの問題を診断する。</td>
-      <td><pre class="inline"><code>$ brew cask doctor</code></pre></td>
     </tr>
   </tbody>
 </table>
