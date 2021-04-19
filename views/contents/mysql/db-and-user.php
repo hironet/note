855208f3fdab6600147d2679fd953702e9710d03
Hiro <hiroyuki1126@gmail.com>
2021-04-19 11:24:55 +0900

    軽微な修正。

--- views/contents/mysql/db-and-user.php
+++ views/contents/mysql/db-and-user.php
@@ -16,14 +16,14 @@ $mtime = get_mtime(__FILE__);
   </li>
 </ul>
 <h2 id="database" class="title">データベース管理</h2>
-<h3 class="title">データベースの作成</h3>
+<h3 class="title">データベース作成</h3>
 <p>1. 作成するデータベースが存在しないことを確認する。</p>
 <pre class="block"><code>SHOW DATABASES;</code></pre>
 <p>2. データベースを作成する。</p>
 <pre class="block"><code>CREATE DATABASE [データベース名] DEFAULT CHARACTER SET [文字コード] COLLATE [照合順序];</code></pre>
 <p>3. 作成したデータベースが存在することを確認する。</p>
 <pre class="block"><code>SHOW DATABASES;</code></pre>
-<h3 class="title">データベースの削除</h3>
+<h3 class="title">データベース削除</h3>
 <p>1. 削除するデータベースが存在することを確認する。</p>
 <pre class="block"><code>SHOW DATABASES;</code></pre>
 <p>2. データベースを削除する。</p>
@@ -31,7 +31,7 @@ $mtime = get_mtime(__FILE__);
 <p>3. 削除したデータベースが存在しないことを確認する。</p>
 <pre class="block"><code>SHOW DATABASES;</code></pre>
 <h2 id="user" class="title">ユーザ管理</h2>
-<h3 class="title">ユーザの作成</h3>
+<h3 class="title">ユーザ作成</h3>
 <p>1. 作成するユーザが存在しないことを確認する。</p>
 <pre class="block"><code>SELECT user, host FROM mysql.user;</code></pre>
 <p>2. ユーザを作成する。</p>
@@ -44,7 +44,7 @@ $mtime = get_mtime(__FILE__);
 <pre class="block"><code>GRANT ALL PRIVILEGES ON [データベース名].* to [ユーザ名];</code></pre>
 <p>6. 作成したユーザに権限が付与されたことを確認する。</p>
 <pre class="block"><code>SHOW GRANTS FOR [ユーザ名];</code></pre>
-<h3 class="title">ユーザの削除</h3>
+<h3 class="title">ユーザ削除</h3>
 <p>1. 削除するユーザが存在することを確認する。</p>
 <pre class="block"><code>SELECT user, host FROM mysql.user;</code></pre>
 <p>2. ユーザを削除する。</p>
