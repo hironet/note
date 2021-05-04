f03affb84fc9a8ad8f4184387f6c621860a54643
Hiro <hiroyuki1126@gmail.com>
2021-05-04 15:42:53 +0900

    内容を修正。

--- views/contents/php/array.php
+++ views/contents/php/array.php
@@ -33,10 +33,10 @@ echo $array[0];  # red</code></pre>
 echo $array[2];  # blue</code></pre>
 <h3 class="title">連想配列の初期化</h3>
 <p>array()で連想配列を初期化する。</p>
-<pre class="block"><code>$array = array('order_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380);
+<pre class="block"><code>$array = array('product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380);
 echo $array['product_name'];  # iPhone 12</code></pre>
 <p>短縮構文で連想配列を初期化する。</p>
-<pre class="block"><code>$array = ['order_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
+<pre class="block"><code>$array = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
 echo $array['price'];  # 94380</code></pre>
 <h2 id="string" class="title">配列と文字列</h2>
 <p>1次元配列の値を文字列内に展開する。</p>
@@ -44,7 +44,7 @@ echo $array['price'];  # 94380</code></pre>
   <li>配列を<code>{}</code>で囲む必要はない。</li>
   <li>キーを<code>'</code>（シングルクォーテーション）で囲んではならない。</li>
 </ul>
-<pre class="block"><code>$array = ['order_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
+<pre class="block"><code>$array = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
 echo "製品名は{$array[product_name]}です。";  # 製品名はiPhone12です。
 echo "製品名は$array[product_name]です。";  # 製品名はiPhone12です。</code></pre>
 <p>多次元配列の値を文字列内に展開する。</p>
@@ -53,14 +53,14 @@ echo "製品名は$array[product_name]です。";  # 製品名はiPhone12です�
   <li>キーを<code>'</code>（シングルクォーテーション）で囲んではならない。</li>
 </ul>
 <pre class="block"><code>$array = [
-  ['order_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380],
-  ['order_no' => 11, 'product_name' => 'iPhone 12 Pro', 'price' => 117,480],
-  ['order_no' => 12, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129,580]
+  ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380],
+  ['product_no' => 11, 'product_name' => 'iPhone 12 Pro', 'price' => 117480],
+  ['product_no' => 12, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129580]
 ];
 echo "製品名は{$array[1][product_name]}です。";  # 製品名はiPhone12 Proです。</code></pre>
 <h2 id="convenient" class="title">配列の便利な操作</h2>
 <h3 class="title">list構文</h3>
 <p>配列から複数の変数に代入する。</p>
-<pre class="block"><code>$color_array = ['red', 'green, 'blue'];
+<pre class="block"><code>$color_array = ['red', 'green', 'blue'];
 list($color_1, $color_2, $color_3) = $color_array;
 echo "${$color_1}, ${$color_2}, ${$color_3}";  # red, green, blue</code></pre>
