1901923bb5169807ae250e2fdb10b78051141e7e
Hiro <hiroyuki1126@gmail.com>
2021-05-29 20:26:44 +0900

    変数名を変更。

--- resources/views/article/subcategory.blade.php
+++ resources/views/article/subcategory.blade.php
@@ -3,12 +3,12 @@
 @section('contents')
 @include('components.header')
 <div class="row">
-@foreach ($article_titles as $_article_id => $article_info)
+@foreach ($article_titles as $_article_id => $_article_info)
   <div class="col-12 col-md-6 col-lg-4">
     <div class="card my-2">
       <div class="card-body">
-        <h5 class="card-title">{{ $article_info[1] }}</h5>
-        <a href="{{ $article_info[0] }}" class="card-link">詳しく見る</a>
+        <h5 class="card-title">{{ $_article_info[1] }}</h5>
+        <a href="{{ $_article_info[0] }}" class="card-link">詳しく見る</a>
       </div>
     </div><!-- card -->
   </div><!-- col -->
