67526a28485c1d4c889b1ff8c1595f87d70c0176
Hiro <hiroyuki1126@gmail.com>
2021-06-19 17:43:30 +0900

    Search Console用のタグを削除。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
 @else
   <meta name="description" content="このサイトは、ITエンジニアHiroの技術メモです。{{ $article_description }}">
 @endempty
-  <meta name="google-site-verification" content="1QRBbOm10zCDZez2WTlhjFb9l22KICKWZlwzT7JTWRI" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
   <link href="{{ secure_asset('/css/common.css') }}" rel="stylesheet">
   <link href="{{ secure_asset('/css/tech-note.css') }}" rel="stylesheet">
