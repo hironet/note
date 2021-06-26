8853e188a6f338528f9202cde11b13d6cfda3750
Hiro <hiroyuki1126@gmail.com>
2021-06-26 22:10:07 +0900

    新規作成。

---
+++ resources/views/article/middleware/docker/docker-compose.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts/app')
+
+@section('contents')
+<header>
+@include('components.header')
+  <nav>
+    <ul class="toc">
+      <li>
+        <a href="#install">Docker Composeのインストール</a>
+      </li>
+      <li>
+        <a href="#image">Dockerイメージの管理</a>
+      </li>
+      <li>
+        <a href="#container">Dockerコンテナの管理</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+<h2 id="install" class="title">Docker Composeのインストール</h2>
+<p><a href="https://github.com/docker/compose/releases" target="_blank" rel="noreferrer noopener">Composeリポジトリ</a>にアクセスし、ページの指示に従ってインストールする。</p>
+<h2 id="image" class="title">Dockerイメージの管理</h2>
+<h3 class="oper">イメージを作成する</h3>
+<pre class="block"><code class="shell">$ docker-compose build</code></pre>
+<h3 class="oper">キャッシュを使わないでイメージを作成する</h3>
+<pre class="block"><code class="shell">$ docker-compose build --no-cache</code></pre>
+<h2 id="container" class="title">Dockerコンテナの管理</h2>
+<h3 class="oper">コンテナの稼働状態を確認する</h3>
+<pre class="block"><code class="shell">$ docker-compose ps</code></pre>
+<h3 class="oper">コンテナを作成する（起動しない）</h3>
+<pre class="block"><code class="shell">$ docker-compose up --no-start</code></pre>
+<h3 class="oper">コンテナを作成・起動する</h3>
+<pre class="block"><code class="shell">$ docker-compose up -d</code></pre>
+<h3 class="oper">コンテナを停止する</h3>
+<pre class="block"><code class="shell">$ docker-compose stop</code></pre>
+<h3 class="oper">コンテナを再起動する</h3>
+<pre class="block"><code class="shell">$ docker-compose restart</code></pre>
+<h3 class="oper">コンテナを削除する</h3>
+<pre class="block"><code class="shell">$ docker-compose down</code></pre>
+<h3 class="oper">データボリュームも含めてコンテナを削除する</h3>
+<pre class="block"><code class="shell">$ docker-compose down -v</code></pre>
+<h3 class="oper">コンテナのログを確認する</h3>
+<pre class="block"><code class="shell">$ docker-compose logs</code></pre>
+@endsection
---
+++ resources/views/article/middleware/docker/network.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts/app')
+
+@section('contents')
+<header>
+@include('components.header')
+  <nav>
+    <ul class="toc">
+      <li>
+        <a href="#basic">基本的な操作</a>
+      </li>
+      <li>
+        <a href="#advanced">高度な操作</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+<h2 id="basic" class="title">基本的な操作</h2>
+<h3 class="oper">Dockerネットワークを確認する</h3>
+<pre class="block"><code class="shell">$ docker network ls</code></pre>
+<h3 class="oper">Dockerネットワークを作成する</h3>
+<pre class="block"><code class="shell">$ docker network create -d bridge [ネットワーク名]</code></pre>
+<h3 class="oper">Dockerネットワークを削除する</h3>
+<pre class="block"><code class="shell">$ docker network rm [ネットワーク名]</code></pre>
+<h3 class="oper">Dockerネットワークを指定する</h3>
+<pre class="block"><code class="shell">$ docker run -itd --name [コンテナ名] --net=[ネットワーク名] [イメージ名]</code></pre>
+<h2 id="advanced" class="title">高度な操作</h2>
+<h3 class="oper">ホストOS宛てのパケットをDockerコンテナにポートフォワーディングする</h3>
+<pre class="block"><code class="shell">$ docker run -itd --name [コンテナ名] -p 8000:80 [イメージ名]</code></pre>
+<ul>
+  <li>ホストOSの8000番ポートをコンテナ内の80番ポートに紐付ける。</li>
+</ul>
+@endsection
