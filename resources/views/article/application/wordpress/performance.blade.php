df78f7cfd9db57ad5c81bf578625b18215f4039d
Hiro <hiroyuki1126@gmail.com>
2021-06-26 23:08:55 +0900

    内容を修正。

--- resources/views/article/application/wordpress/performance.blade.php
+++ resources/views/article/application/wordpress/performance.blade.php
@@ -3,15 +3,28 @@
 @section('contents')
 <header>
 @include('components.header')
+  <nav>
+    <ul class="toc">
+      <li>
+        <a href="#revision">リビジョン削除</a>
+      </li>
+      <li>
+        <a href="#fragment">wp-postsテーブルのフラグメント化解消</a>
+      </li>
+    </ul>
+  </nav>
 </header>
-<h3 class="title">リビジョン削除</h3>
-<p>データベース（MySQL）に接続し、以下SQLを実行する。</p>
+<h2 id="revision" class="title">リビジョン削除</h2>
+<p>1. データベース（MySQL）に接続する。</p>
+<p>2. 以下SQLを実行する。</p>
 <pre class="block"><code class="sql">USE wordpress;
 SELECT COUNT(*) FROM wp_posts WHERE post_type='revision';
 DELETE FROM wp_posts WHERE post_type='revision';
 SELECT COUNT(*) FROM wp_posts WHERE post_type='revision';</code></pre>
-<h3 class="title">wp-postsテーブルのフラグメント化解消</h3>
-<p>データベース（MySQL）に接続し、リビジョンを削除した後、以下SQLを実行する。</p>
+<h2 id="fragment" class="title">wp-postsテーブルのフラグメント化解消</h2>
+<p>1. データベース（MySQL）に接続する。</p>
+<p>2. リビジョンを削除する。</p>
+<p>3. 以下SQLを実行する。</p>
 <pre class="block"><code class="sql">USE wordpress;
 SELECT @@sql_mode;
 SET sql_mode = '';
