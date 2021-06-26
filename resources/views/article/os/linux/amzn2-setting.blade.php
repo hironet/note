9651eacf17ba8d4570dbb7aff88cfd1fe5657601
Hiro <hiroyuki1126@gmail.com>
2021-06-26 16:50:01 +0900

    内容を修正。

--- resources/views/article/os/linux/amzn2-setting.blade.php
+++ resources/views/article/os/linux/amzn2-setting.blade.php
@@ -33,105 +33,52 @@
   </nav>
 </header>
 <h2 id="yum" class="title">パッケージの管理（Yum）</h2>
-<div class="table-responsive">
-  <table class="table table-striped table-sm">
-    <thead>
-      <tr>
-        <th>操作</th>
-        <th>コマンド</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>リポジトリ一覧を表示する。</td>
-        <td><pre class="inline"><code class="shell"># yum repolist</code></pre></td>
-      </tr>
-      <tr>
-        <td>パッケージ情報を検索する。</td>
-        <td><pre class="inline"><code class="shell"># yum search [キーワード]</code></pre></td>
-      </tr>
-      <tr>
-        <td>インストール可能なパッケージ一覧を表示する。</td>
-        <td><pre class="inline"><code class="shell"># yum list</code></pre></td>
-      </tr>
-      <tr>
-        <td>インストールされているパッケージ一覧を表示する。</td>
-        <td><pre class="inline"><code class="shell"># yum list installed</code></pre></td>
-      </tr>
-      <tr>
-        <td>パッケージをインストールする。</td>
-        <td><pre class="inline"><code class="shell"># yum -y install [パッケージ名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>パッケージをアンインストールする。</td>
-        <td><pre class="inline"><code class="shell"># yum remove [パッケージ名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>インストールされている全てのパッケージを更新する。</td>
-        <td><pre class="inline"><code class="shell"># yum update</code></pre></td>
-      </tr>
-      <tr>
-        <td>インストールされている個別のパッケージを更新する。</td>
-        <td><pre class="inline"><code class="shell"># yum update [パッケージ名]</code></pre></td>
-      </tr>
-    </tbody>
-  </table>
-</div><!-- /.table-responsive -->
-<p>yumでインストールされたファイル一覧を表示する。</p>
-<ul>
-  <li>事前に、<code>yum-utils</code>パッケージをインストールしておく必要がある。</li>
-</ul>
+<h3 class="oper">リポジトリ一覧を表示する</h3>
+<pre class="block"><code class="shell"># yum repolist</code></pre>
+<h3 class="oper">パッケージを検索する</h3>
+<pre class="block"><code class="shell"># yum search [キーワード]</code></pre>
+<h3 class="oper">インストール可能なパッケージ一覧を表示する</h3>
+<pre class="block"><code class="shell"># yum list</code></pre>
+<h3 class="oper">インストールされているパッケージ一覧を表示する</h3>
+<pre class="block"><code class="shell"># yum list installed</code></pre>
+<h3 class="oper">パッケージをインストールする</h3>
+<pre class="block"><code class="shell"># yum -y install [パッケージ名]</code></pre>
+<h3 class="oper">パッケージをアンインストールする</h3>
+<pre class="block"><code class="shell"># yum remove [パッケージ名]</code></pre>
+<h3 class="oper">インストールされている全てのパッケージを更新する</h3>
+<pre class="block"><code class="shell"># yum update</code></pre>
+<h3 class="oper">インストールされている個別のパッケージを更新する</h3>
+<pre class="block"><code class="shell"># yum update [パッケージ名]</code></pre>
+<h3 class="oper">yumでインストールされたファイル一覧を表示する</h3>
 <pre class="block"><code class="shell"># repoquery --list [パッケージ名]</code></pre>
-<p>RPMパッケージをダウンロードする。</p>
 <ul>
   <li>事前に、<code>yum-utils</code>パッケージをインストールしておく必要がある。</li>
 </ul>
+<h3 class="oper">RPMパッケージをダウンロードする</h3>
 <pre class="block"><code class="shell"># yumdownloader --resolve [パッケージ名]</code></pre>
+<ul>
+  <li>事前に、<code>yum-utils</code>パッケージをインストールしておく必要がある。</li>
+</ul>
 <h2 id="rpm" class="title">パッケージの管理（RPM）</h2>
-<p>ファイルがどのパッケージに含まれるかを確認する。</p>
+<h3 class="oper">ファイルがどのパッケージに含まれるかを確認する</h3>
 <pre class="block"><code class="shell"># rpm -qf [ファイルパス]</code></pre>
 <h2 id="service" class="title">サービスの操作</h2>
-<div class="table-responsive">
-  <table class="table table-striped table-sm">
-    <thead>
-      <tr>
-        <th>操作</th>
-        <th>コマンド</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>サービス一覧を表示する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl list-unit-files -t service</code></pre></td>
-      </tr>
-      <tr>
-        <td>サービスの状態を確認する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl status [unit名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>サービスを起動する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl start [unit名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>サービスを停止する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl stop [unit名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>サービスを再起動する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl restart [unit名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>サービスの自動起動を有効化する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl enable [unit名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>サービスの自動起動を無効化する。</td>
-        <td><pre class="inline"><code class="shell"># systemctl disable [unit名]</code></pre></td>
-      </tr>
-    </tbody>
-  </table>
-</div><!-- /.table-responsive -->
+<h3 class="oper">サービス一覧を表示する</h3>
+<pre class="block"><code class="shell"># systemctl list-unit-files -t service</code></pre>
+<h3 class="oper">サービスの状態を確認する</h3>
+<pre class="block"><code class="shell"># systemctl status [unit名]</code></pre>
+<h3 class="oper">サービスを起動する</h3>
+<pre class="block"><code class="shell"># systemctl start [unit名]</code></pre>
+<h3 class="oper">サービスを停止する</h3>
+<pre class="block"><code class="shell"># systemctl stop [unit名]</code></pre>
+<h3 class="oper">サービスを再起動する</h3>
+<pre class="block"><code class="shell"># systemctl restart [unit名]</code></pre>
+<h3 class="oper">サービスの自動起動を有効化する</h3>
+<pre class="block"><code class="shell"># systemctl enable [unit名]</code></pre>
+<h3 class="oper">サービスの自動起動を無効化する</h3>
+<pre class="block"><code class="shell"># systemctl disable [unit名]</code></pre>
 <h2 id="swap" class="title">スワップ領域の設定</h2>
+<h3 class="oper">スワップ領域を作成する</h3>
 <p>1. 2GB（128MB×16）のスワップファイル（<code>/swapfile</code>）を作成する。</p>
 <pre class="block"><code class="shell"># dd if=/dev/zero of=/swapfile bs=128M count=16</code></pre>
 <p>2. スワップファイルのアクセス権を変更する。</p>
@@ -164,14 +111,14 @@
 <p>3. タイムゾーンが<code>Asia/Tokyo (JST)</code>に設定されたことを確認する。</p>
 <pre class="block"><code class="shell"># timedatectl status</code></pre>
 <h2 id="other" class="title">その他</h2>
-<p>OS起動時にコマンドを自動実行する。</p>
+<h3 class="oper">OS起動時にコマンドを自動実行する</h3>
 <pre class="block"><code class="shell"># cat /etc/rc.d/rc.local
 [実行したいコマンド]
 # chmod +x /etc/rc.d/rc.local</code></pre>
-<p>OSログイン時にメッセージを表示する。</p>
+<h3 class="oper">OSログイン時にメッセージを表示する</h3>
 <pre class="block"><code class="shell"># cat /etc/motd
 [表示したいメッセージ]</code></pre>
-<p>HTTPプロキシを設定する。</p>
+<h3 class="oper">HTTPプロキシを設定する</h3>
 <pre class="block"><code class="shell"># cat /etc/profile.d/proxy.sh
 export http_proxy=[プロキシのホスト名またはIPアドレス]:[ポート番号]
 export HTTP_PROXY=$http_proxy
@@ -179,13 +126,13 @@ export https_proxy=$http_proxy
 export HTTPS_PROXY=$http_proxy
 export no_proxy=127.0.0.1,localhost
 export NO_PROXY=$no_proxy</code></pre>
-<p>cronで実行したコマンドの標準出力および標準エラー出力がメールで飛ばないようにする。</p>
+<h3 class="oper">cronで実行したコマンドの標準出力および標準エラー出力がメールで飛ばないようにする</h3>
 <pre class="block"><code class="shell">$ crontab -l
 [分] [時] [日] [月] [曜日] [コマンド] &gt; /dev/null 2&gt;&amp;1</code></pre>
-<p>フォーマット付きのdateコマンドをcronで実行する。</p>
+<h3 class="oper">フォーマット付きのdateコマンドをcronで実行する</h3>
+<pre class="block"><code class="shell">$ crontab -l
+[分] [時] [日] [月] [曜日] [コマンド] &gt; /tmp/output-$(date '+\%Y\%m\%d').log</code></pre>
 <ul>
   <li>フォーマット中の<code>%</code>は、<code>\</code>でエスケープする必要がある点に注意。</li>
 </ul>
-<pre class="block"><code class="shell">$ crontab -l
-[分] [時] [日] [月] [曜日] [コマンド] &gt; /tmp/output-$(date '+\%Y\%m\%d').log</code></pre>
 @endsection
--- resources/views/article/os/linux/command.blade.php
+++ resources/views/article/os/linux/command.blade.php
@@ -107,61 +107,61 @@
     </ul>
   </nav>
 </header>
-<h3 id="arping" class="title">arping</h3>
-<p>同一セグメント上で、pingに応答しないノードの存在を確認する。</p>
+<h2 id="arping" class="title">arping</h2>
+<h3 class="oper">同一セグメント上で、pingに応答しないノードの存在を確認する</h3>
 <pre class="block"><code class="shell">$ arping [IPアドレス]</code></pre>
-<p>IPアドレスの重複が発生していないかをGratuitous ARPで確認する。</p>
+<h3 class="oper">IPアドレスの重複が発生していないかをGratuitous ARPで確認する</h3>
 <pre class="block"><code class="shell">$ arping -D [IPアドレス]</code></pre>
-<p>IPアドレスの変更をGratuitous ARPで他のノードに通知し、MACアドレステーブルおよびARPテーブルを更新させる。</p>
+<h3 class="oper">IPアドレスの変更をGratuitous ARPで他のノードに通知し、MACアドレステーブルおよびARPテーブルを更新させる</h3>
 <pre class="block"><code class="shell">$ arping -A [IPアドレス]</code></pre>
-<h3 id="awk" class="title">awk</h3>
-<p>ファイルの2列目のみ表示する。</p>
+<h2 id="awk" class="title">awk</h2>
+<h3 class="oper">ファイルの2列目のみ表示する</h3>
 <pre class="block"><code class="shell">$ cat [ファイルパス] | awk '{ print $2 }'</code></pre>
-<p>ファイルの2列目の値が50以上の行のみ表示する。</p>
+<h3 class="oper">ファイルの2列目の値が50以上の行のみ表示する</h3>
 <pre class="block"><code class="shell">$ cat [ファイルパス] | awk '{ if ($2 >= 50) print $0 }'</code></pre>
-<p>ファイルの各レコードが","で区切られており、5フィールド目の値が"ok"、かつ、6フィールド目の値が"dev"にマッチする行のみ表示する。</p>
+<h3 class="oper">ファイルの各レコードが","で区切られており、5フィールド目の値が"ok"、かつ、6フィールド目の値が"dev"にマッチする行のみ表示する</h3>
 <pre class="block"><code class="shell">$ cat [ファイルパス] | awk -F ',' '{ if ($5 == "ok" &amp;&amp; $6 ~ /dev/) print $0 }'</code></pre>
-<p>ログファイルから特定の時間帯の行のみ表示する。</p>
+<h3 class="oper">ログファイルから特定の時間帯の行のみ表示する</h3>
 <pre class="block"><code class="shell">$ awk -F - '"2018/07/20 03:30:00" &lt; $1 &amp;&amp; $1 &lt;= "2018/07/20 03:35:00"' [ファイルパス]</code></pre>
-<h3 id="basename" class="title">basename</h3>
-<p>ファイルパスからファイル名部分を取り出す。</p>
+<h2 id="basename" class="title">basename</h2>
+<h3 class="oper">ファイルパスからファイル名部分を取り出す</h3>
 <pre class="block"><code class="shell">$ basename /var/log/messages
 messages</code></pre>
-<h3 id="chmod" class="title">chmod</h3>
-<p>ディレクトリにSticky Bitを設定する。</p>
+<h2 id="chmod" class="title">chmod</h2>
+<h3 class="oper">ディレクトリにSticky Bitを設定する</h3>
+<pre class="block"><code class="shell">$ chmod +t [ディレクトリパス]</code></pre>
 <ul>
   <li>Sticky Bitが設定されたディレクトリでは、全てのユーザがディレクトリおよびファイルを書き込みできるが、所有者のみが削除を行える。</li>
 </ul>
-<pre class="block"><code class="shell">$ chmod +t [ディレクトリパス]</code></pre>
-<h3 id="chown" class="title">chown</h3>
-<p>シンボリックリンクの所有者・所有グループを変更する。</p>
+<h2 id="chown" class="title">chown</h2>
+<h3 class="oper">シンボリックリンクの所有者・所有グループを変更する</h3>
 <pre class="block"><code class="shell">$ chown -h [所有者]:[所有グループ] [ファイルパス]</code></pre>
-<h3 id="curl" class="title">curl</h3>
-<p>WebサーバからHTTPヘッダとデータを取得する。</p>
+<h2 id="curl" class="title">curl</h2>
+<h3 class="oper">WebサーバからHTTPヘッダとデータを取得する</h3>
 <pre class="block"><code class="shell">$ curl -i [URL]</code></pre>
-<p>WebサーバからHTTPヘッダのみを取得する。</p>
+<h3 class="oper">WebサーバからHTTPヘッダのみを取得する</h3>
 <pre class="block"><code class="shell">$ curl -I [URL]</code></pre>
-<p>SSLサーバ証明書に問題のあるWebサーバからデータを取得する。</p>
+<h3 class="oper">SSLサーバ証明書に問題のあるWebサーバからデータを取得する</h3>
 <pre class="block"><code class="shell">$ curl -k [URL]</code></pre>
-<h3 id="dd" class="title">dd</h3>
-<p>I/O負荷をかける。</p>
+<h2 id="dd" class="title">dd</h2>
+<h3 class="oper">I/O負荷をかける</h3>
 <pre class="block"><code class="shell">$ dd if=/dev/zero of=test bs=1M count=512 oflag=direct</code></pre>
-<h3 id="dirname" class="title">dirname</h3>
-<p>ファイルパスからディレクトリ部分を取り出す。</p>
+<h2 id="dirname" class="title">dirname</h2>
+<h3 class="oper">ファイルパスからディレクトリ部分を取り出す</h3>
 <pre class="block"><code class="shell">$ dirname /var/log/messages
 /var/log</code></pre>
-<h3 id="dmidecode" class="title">dmidecode</h3>
-<p>ハードウェア情報を表示する。</p>
+<h2 id="dmidecode" class="title">dmidecode</h2>
+<h3 class="oper">ハードウェア情報を表示する</h3>
 <pre class="block"><code class="shell">$ dmidecode</code></pre>
-<h3 id="echo" class="title">echo</h3>
-<p>空ファイルを作成する。</p>
+<h2 id="echo" class="title">echo</h2>
+<h3 class="oper">空ファイルを作成する</h3>
 <pre class="block"><code class="shell">$ echo -n > [ファイルパス]</code></pre>
-<p><code>\n</code>の位置で改行する。</p>
+<h3 class="oper"><code>\n</code>の位置で改行する</h3>
 <pre class="block"><code class="shell">$ echo -e 'hello\nworld'
 hello
 world</code></pre>
-<h3 id="expr" class="title">expr</h3>
-<p>四則演算をする。</p>
+<h2 id="expr" class="title">expr</h2>
+<h3 class="oper">四則演算をする</h3>
 <pre class="block"><code class="shell">$ expr 100 + 200
 300
 $ expr 300 - 200
@@ -170,129 +170,129 @@ $ expr 11 '*' 11
 121
 $ expr 400 / 2
 200</code></pre>
-<h3 id="find" class="title">find</h3>
-<p>所有者がrootまたは所有グループがrootのファイルを検索する。</p>
+<h2 id="find" class="title">find</h2>
+<h3 class="oper">所有者がrootまたは所有グループがrootのファイルを検索する</h3>
 <pre class="block"><code class="shell">$ find [ディレクトリパス] \( -user root -o -group root \)</code></pre>
-<p>所有者が存在しないファイルを検索する。</p>
+<h3 class="oper">所有者が存在しないファイルを検索する</h3>
 <pre class="block"><code class="shell">$ find / -nouser</code></pre>
-<p>所有グループが存在しないファイルを検索する。</p>
+<h3 class="oper">所有グループが存在しないファイルを検索する</h3>
 <pre class="block"><code class="shell">$ find / -nogroup</code></pre>
-<p>更新日時が指定した時刻より新しいファイルを検索する。</p>
+<h3 class="oper">更新日時が指定した時刻より新しいファイルを検索する</h3>
 <pre class="block"><code class="shell">$ find [ディレクトリパス] -newermt 'YYYYMMDD HH:MM'</code></pre>
-<p>特定のディレクトリ（<code>/home</code>および<code>/proc</code>）配下を検索しないようにする。</p>
+<h3 class="oper">特定のディレクトリ（<code>/home</code>および<code>/proc</code>）配下を検索しないようにする</h3>
 <pre class="block"><code class="shell">$ find [ディレクトリパス] -not -path '/home/*' -not -path '/proc/*'</code></pre>
-<p>検索結果のファイル名だけ取り出す。</p>
+<h3 class="oper">検索結果のファイル名だけ取り出す</h3>
 <pre class="block"><code class="shell">$ find [ディレクトリパス] -type f -exec basename {} \;</code></pre>
-<p>ディレクトリのパーミッションを再帰的に変更する。</p>
+<h3 class="oper">ディレクトリのパーミッションを再帰的に変更する</h3>
 <pre class="block"><code class="shell">$ find [ディレクトリパス] -type d -exec chmod 755 {} \;</code></pre>
-<h3 id="grep" class="title">grep</h3>
-<p>パターン文字列にマッチした前後の行も表示する。</p>
+<h2 id="grep" class="title">grep</h2>
+<h3 class="oper">パターン文字列にマッチした前後の行も表示する</h3>
 <pre class="block"><code class="shell">$ grep -C [行数] [パターン文字列] [ファイルパス]</code></pre>
-<p>設定ファイルのコメントと空行以外を表示する。</p>
+<h3 class="oper">設定ファイルのコメントと空行以外を表示する</h3>
 <pre class="block"><code class="shell">$ grep -v -e '^\s*#' -e '^\s*$' [ファイルパス]</code></pre>
-<p>パターン文字列のリストをファイルから読み込む。</p>
+<h3 class="oper">パターン文字列のリストをファイルから読み込む</h3>
 <pre class="block"><code class="shell">$ grep -f [パターン文字列を記載したファイル] [ファイルパス]</code></pre>
-<p>ハイフンから始まる文字列を検索する。</p>
+<h3 class="oper">ハイフンから始まる文字列を検索する</h3>
 <pre class="block"><code class="shell">$ grep -- [ハイフンから始まるパターン文字列] [ファイルパス]</code></pre>
-<p>psコマンドのヘッダを表示し、grep自身は除いて表示する。</p>
+<h3 class="oper">psコマンドのヘッダを表示し、grep自身は除いて表示する</h3>
 <pre class="block"><code class="shell">$ ps aux | grep -e [U]SER -e [パターン文字列]</code></pre>
-<h3 id="ip" class="title">ip</h3>
-<p>指定したIPアドレスに到達するための経路を表示する。</p>
+<h2 id="ip" class="title">ip</h2>
+<h3 class="oper">指定したIPアドレスに到達するための経路を表示する</h3>
 <pre class="block"><code class="shell">$ ip route get [IPアドレス]</code></pre>
-<p>ARPテーブルのエントリを表示する。</p>
+<h3 class="oper">ARPテーブルのエントリを表示する</h3>
 <pre class="block"><code class="shell">$ ip n</code></pre>
-<h3 id="lsblk" class="title">lsblk</h3>
-<p>ブロックデバイスを一覧表示する。</p>
+<h2 id="lsblk" class="title">lsblk</h2>
+<h3 class="oper">ブロックデバイスを一覧表示する</h3>
 <pre class="block"><code class="shell">$ lsblk</code></pre>
-<h3 id="lsof" class="title">lsof</h3>
-<p>ファイルを使用しているプロセスを表示する。</p>
+<h2 id="lsof" class="title">lsof</h2>
+<h3 class="oper">ファイルを使用しているプロセスを表示する</h3>
 <pre class="block"><code class="shell">$ lsof [ファイルパス]</code></pre>
-<p>ポートを使用しているプロセスを表示する。</p>
+<h3 class="oper">ポートを使用しているプロセスを表示する</h3>
 <pre class="block"><code class="shell">$ lsof -i:[ポート番号]</code></pre>
-<p>プロセスが使用しているファイルを表示する。</p>
+<h3 class="oper">プロセスが使用しているファイルを表示する</h3>
 <pre class="block"><code class="shell">$ lsof -p [プロセスIDまたはプロセス名]</code></pre>
-<p>ユーザが使用しているファイルを表示する。</p>
+<h3 class="oper">ユーザが使用しているファイルを表示する</h3>
 <pre class="block"><code class="shell">$ lsof -u [ユーザ名]</code></pre>
-<h3 id="mkpasswd" class="title">mkpasswd</h3>
-<p>10文字のパスワードを生成する。</p>
+<h2 id="mkpasswd" class="title">mkpasswd</h2>
+<h3 class="oper">10文字のパスワードを生成する</h3>
 <pre class="block"><code class="shell">$ mkpasswd -l 10</code></pre>
-<h3 id="mktemp" class="title">mktemp</h3>
-<p>一時ファイルを作成する。</p>
+<h2 id="mktemp" class="title">mktemp</h2>
+<h3 class="oper">一時ファイルを作成する</h3>
 <pre class="block"><code class="shell">$ mktemp</code></pre>
-<p>一時ディレクトリを作成する。</p>
+<h3 class="oper">一時ディレクトリを作成する</h3>
 <pre class="block"><code class="shell">$ mktemp -d</code></pre>
-<h3 id="nc" class="title">nc</h3>
-<p>適当なポートをLISTEN状態にする。</p>
+<h2 id="nc" class="title">nc</h2>
+<h3 class="oper">適当なポートをLISTEN状態にする</h3>
+<pre class="block"><code class="shell">$ nc -lkv -p [ポート番号]</code></pre>
 <ul>
   <li><code>-k</code>オプション … 複数コネクションを受け付ける。</li>
   <li><code>-v</code>オプション … 詳細情報を出力する。</li>
 </ul>
-<pre class="block"><code class="shell">$ nc -lkv -p [ポート番号]</code></pre>
-<p>サーバに接続する。</p>
+<h3 class="oper">サーバに接続する</h3>
+<pre class="block"><code class="shell">$ nc -zv [宛先ホスト] [ポート番号]</code></pre>
 <ul>
   <li><code>-z</code>オプション … 接続した後、すぐに切断する。</li>
 </ul>
-<pre class="block"><code class="shell">$ nc -zv [宛先ホスト] [ポート番号]</code></pre>
-<p>簡易Webサーバを立ち上げる。</p>
+<h3 class="oper">簡易Webサーバを立ち上げる</h3>
 <pre class="block"><code class="shell">$ while true; do ( echo -e 'HTTP/1.1 200 OK\n\nhello'; ) | nc -l 8000; done</code></pre>
 <pre class="block"><code class="shell">$ while true; do ( echo -e 'HTTP/1.1 200 OK\n\n'; cat index.html; ) | nc -l 8000; done</code></pre>
-<h3 id="nohup" class="title">nohup</h3>
-<p>ログアウト後もコマンドを実行し続ける。</p>
+<h2 id="nohup" class="title">nohup</h2>
+<h3 class="oper">ログアウト後もコマンドを実行し続ける</h3>
 <pre class="block"><code class="shell">$ nohup [コマンド] &amp;</code></pre>
-<h3 id="readlink" class="title">readlink</h3>
-<p>相対パスを絶対パスに変換する。</p>
+<h2 id="readlink" class="title">readlink</h2>
+<h3 class="oper">相対パスを絶対パスに変換する</h3>
 <pre class="block"><code class="shell">$ readlink -f [相対パス]</code></pre>
-<h3 id="sed" class="title">sed</h3>
-<p>ファイルの5行目を表示する。</p>
+<h2 id="sed" class="title">sed</h2>
+<h3 class="oper">ファイルの5行目を表示する</h3>
 <pre class="block"><code class="shell">$ sed -n 5p [ファイルパス]</code></pre>
-<p>ファイルの10～20行目を表示する。</p>
+<h3 class="oper">ファイルの10～20行目を表示する</h3>
 <pre class="block"><code class="shell">$ sed -n 10,20p [ファイルパス]</code></pre>
-<p>ファイルの5行目を削除する。</p>
+<h3 class="oper">ファイルの5行目を削除する</h3>
 <pre class="block"><code class="shell">$ sed -e '5d' [ファイルパス]</code></pre>
-<p>ファイルの10～20行目を削除する。</p>
+<h3 class="oper">ファイルの10～20行目を削除する</h3>
 <pre class="block"><code class="shell">$ sed -e '10,20d' [ファイルパス]</code></pre>
-<p>特定の文字列を含む行を削除する。</p>
+<h3 class="oper">特定の文字列を含む行を削除する</h3>
+<pre class="block"><code class="shell">$ sed -i -e '/[文字列>/d' [ファイルパス]</code></pre>
 <ul>
   <li><code>-i</code>オプション … ファイルを直接編集する。</li>
 </ul>
-<pre class="block"><code class="shell">$ sed -i -e '/[文字列>/d' [ファイルパス]</code></pre>
-<p>特定の文字列全てを別の文字列に置換する。</p>
+<h3 class="oper">特定の文字列全てを別の文字列に置換する</h3>
 <pre class="block"><code class="shell">$ sed -e 's/[置換前の文字列>/[置換後の文字列>/g' [ファイルパス]</code></pre>
-<h3 id="snmpwalk" class="title">snmpwalk</h3>
-<p>全MIBを取得する。</p>
+<h2 id="snmpwalk" class="title">snmpwalk</h2>
+<h3 class="oper">全MIBを取得する</h3>
 <pre class="block"><code class="shell">$ snmpwalk -v 2c -c [コミュニティ名] [IPアドレス]</code></pre>
-<h3 id="ssh" class="title">ssh</h3>
-<p>データベースにないホストへの接続時に警告メッセージを表示しない。</p>
+<h2 id="ssh" class="title">ssh</h2>
+<h3 class="oper">データベースにないホストへの接続時に警告メッセージを表示しない</h3>
 <pre class="block"><code class="shell">$ ssh -o StrictHostKeyChecking=no [ユーザ名]@[ホスト名またはIPアドレス]</code></pre>
-<p>ローカルのシェルスクリプトをリモートホストで実行する。</p>
+<h3 class="oper">ローカルのシェルスクリプトをリモートホストで実行する</h3>
 <pre class="block"><code class="shell">$ cat [ファイルパス] | ssh [ユーザ名]@[ホスト名またはIPアドレス] bash</code></pre>
-<h3 id="strace" class="title">strace</h3>
-<p>プロセスが呼び出すシステムコールをトレースする。</p>
+<h2 id="strace" class="title">strace</h2>
+<h3 class="oper">プロセスが呼び出すシステムコールをトレースする</h3>
 <pre class="block"><code class="shell">$ strace [コマンド]</code></pre>
-<p>既に実行されているプロセスが呼び出すシステムコールをトレースする。</p>
-<pre class="block"><code class="shell">$ strace -p [プロセスID]</code></pre>
-<h3 id="tail" class="title">tail</h3>
-<p>ファイルの2行目以降を表示する。</p>
+<h3 class="oper">既に実行されているプロセスが呼び出すシステムコールをトレースする</h3>
+<pre class="block"><code class="shell">$ strace -h3 [プロセスID]</code></pre>
+<h2 id="tail" class="title">tail</h2>
+<h3 class="oper">ファイルの2行目以降を表示する</h3>
 <pre class="block"><code class="shell">$ tail -n +2 [ファイルパス]</code></pre>
-<h3 id="tar" class="title">tar</h3>
-<p><code>tar.gz</code>ファイルに格納されているファイル一覧を表示する。</p>
+<h2 id="tar" class="title">tar</h2>
+<h3 class="oper">tar.gzファイルに格納されているファイル一覧を表示する</h3>
 <pre class="block"><code class="shell">$ tar ztvf [ファイルパス]</code></pre>
-<h3 id="tcpdump" class="title">tcpdump</h3>
-<p>Wiresharkを使って解析するためのキャプチャファイルを取得する。</p>
+<h2 id="tcpdump" class="title">tcpdump</h2>
+<h3 class="oper">Wiresharkを使って解析するためのキャプチャファイルを取得する</h3>
 <pre class="block"><code class="shell">$ tcpdump -n -i [ネットワークインタフェース名] -s 0 not port 22 -w [保存先のファイルパス]</code></pre>
-<h3 id="tcpslice" class="title">tcpslice</h3>
-<p>tcpdumpのファイルがいつからいつまでのデータを含んでいるかを確認する。</p>
+<h2 id="tcpslice" class="title">tcpslice</h2>
+<h3 class="oper">tcpdumpのファイルがいつからいつまでのデータを含んでいるかを確認する</h3>
 <pre class="block"><code class="shell">$ tcpslice -t [tcpdumpのファイルパス]</code></pre>
-<p>tcpdumpのファイルから、2018/07/24 02:50:00～3600秒後の範囲を抽出する。</p>
+<h3 class="oper">tcpdumpのファイルから、2018/07/24 02:50:00～3600秒後の範囲を抽出する</h3>
 <pre class="block"><code class="shell">$ tcpslice -w [保存先のファイルパス] 18y07m24d02h50m00s +3600 [tcpdumpのファイルパス]</code></pre>
-<h3 id="tee" class="title">tee</h3>
-<p>標準入力から読み込んだ内容を標準出力とファイルに出力する。</p>
+<h2 id="tee" class="title">tee</h2>
+<h3 class="oper">標準入力から読み込んだ内容を標準出力とファイルに出力する</h3>
+<pre class="block"><code class="shell">$ [コマンド] 2&gt;&amp;1 | tee [オプション] [ファイルパス]</code></pre>
 <ul>
   <li><code>2&gt;&amp;1</code>を付けることで、標準エラー出力も<code>tee</code>に読み込ませることができる。</li>
   <li><code>-a</code>オプション … 指定したファイルに追記する。</li>
 </ul>
-<pre class="block"><code class="shell">$ [コマンド] 2&gt;&amp;1 | tee [オプション] [ファイルパス]</code></pre>
-<h3 id="test" class="title">test</h3>
+<h2 id="test" class="title">test</h2>
 <pre class="block"><code class="shell">$ test [オプション] [文字列またはファイルパス]</code></pre>
 <div class="table-responsive">
   <table class="table table-striped table-sm">
@@ -342,10 +342,13 @@ $ expr 400 / 2
     </tbody>
   </table>
 </div><!-- /.table-responsive -->
-<h3 id="view" class="title">view</h3>
-<p>viをreadonlyモードで起動する（<code>vi -R</code>と同じ）。</p>
+<h2 id="view" class="title">view</h2>
+<h3 class="oper">viをreadonlyモードで起動する</h3>
 <pre class="block"><code class="shell">$ view [ファイルパス]</code></pre>
-<h3 id="yes" class="title">yes</h3>
-<p>CPUに高い負荷をかける。</p>
+<ul>
+  <li><code>vi -R</code>と同じ。</li>
+</ul>
+<h2 id="yes" class="title">yes</h2>
+<h3 class="oper">CPUに高い負荷をかける</h3>
 <pre class="block"><code class="shell">$ yes > /dev/null</code></pre>
 @endsection
