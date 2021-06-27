8089ff147e6ff3bff20d961e03ef73d59c7e7071
Hiro <hiroyuki1126@gmail.com>
2021-06-27 11:56:48 +0900

    内容を修正。

--- resources/views/article/tool/office/setting.blade.php
+++ resources/views/article/tool/office/setting.blade.php
@@ -3,26 +3,23 @@
 @section('contents')
 <header>
 @include('components.header')
+  <nav>
+    <ul class="toc">
+      <li>
+        <a href="#option">Excelのオプション</a>
+      </li>
+    </ul>
+  </nav>
 </header>
-<h2 class="title">Excelのオプション</h2>
-<p>ユーザー名を設定する。</p>
-<figure class="figure">
-  <img class="figure-img" src="/img/office/excel2019-username.webp" alt="ユーザー名の設定">
-</figure>
-<p>オートコレクトを無効にする。</p>
-<figure class="figure">
-  <img class="figure-img" src="/img/office/excel2019-auto-correct.webp" alt="オートコレクトの設定">
-</figure>
-<p>アドレスをハイパーリンクに変更しない。</p>
-<figure class="figure">
-  <img class="figure-img" src="/img/office/excel2019-input-auto-format.webp" alt="入力オートフォーマットの設定">
-</figure>
-<p>オートコンプリートを無効にする。</p>
-<figure class="figure">
-  <img class="figure-img" src="/img/office/excel2019-auto-complete.webp" alt="オートコンプリートの設定">
-</figure>
-<p>クイックアクセスツールバーを設定する。</p>
-<figure class="figure">
-  <img class="figure-img" src="/img/office/excel2019-quick-access.webp" alt="クイックアクセスツールバーの設定">
-</figure>
+<h2 id="option" class="title">Excelのオプション</h2>
+<h3 class="oper">ユーザー名を設定する</h3>
+<img class="figure-img" src="/img/office/excel2019-username.webp" alt="ユーザー名の設定">
+<h3 class="oper">オートコレクトを無効にする</h3>
+<img class="figure-img" src="/img/office/excel2019-auto-correct.webp" alt="オートコレクトの設定">
+<h3 class="oper">アドレスをハイパーリンクに変更しない</h3>
+<img class="figure-img" src="/img/office/excel2019-input-auto-format.webp" alt="入力オートフォーマットの設定">
+<h3 class="oper">オートコンプリートを無効にする</h3>
+<img class="figure-img" src="/img/office/excel2019-auto-complete.webp" alt="オートコンプリートの設定">
+<h3 class="oper">クイックアクセスツールバーを設定する</h3>
+<img class="figure-img" src="/img/office/excel2019-quick-access.webp" alt="クイックアクセスツールバーの設定">
 @endsection
