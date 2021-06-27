3225d77cf92cb98ea29105af9af4cb12c9055f88
Hiro <hiroyuki1126@gmail.com>
2021-06-27 11:51:36 +0900

    内容を修正。

--- resources/views/article/tool/vscode/keyboard.blade.php
+++ resources/views/article/tool/vscode/keyboard.blade.php
@@ -26,9 +26,6 @@
       <li>
         <a href="#emmet">Emmet</a>
       </li>
-      <li>
-        <a href="#browser">open in browser</a>
-      </li>
     </ul>
   </nav>
 </header>
@@ -44,12 +41,12 @@
     </thead>
     <tbody>
       <tr>
-        <td>コマンドパレットを開く。</td>
+        <td>コマンドパレットを開く</td>
         <td><kbd>Ctrl + Shift + P</kbd></td>
         <td><kbd>&#8984; + &#8679; + P</kbd></td>
       </tr>
       <tr>
-        <td>設定画面を開く。</td>
+        <td>設定画面を開く</td>
         <td><kbd>Ctrl + ,</kbd></td>
         <td><kbd>&#8984; + ,</kbd></td>
       </tr>
@@ -68,48 +65,48 @@
     </thead>
     <tbody>
       <tr>
-        <td>新規ファイルを作成する。</td>
+        <td>新規ファイルを作成する</td>
         <td><kbd>Ctrl + N</kbd></td>
         <td><kbd>&#8984; + N</kbd></td>
       </tr>
       <tr>
-        <td>ファイルを開く。</td>
+        <td>ファイルを開く</td>
         <td><kbd>Ctrl + O</kbd></td>
         <td><kbd>&#8984; + O</kbd></td>
       </tr>
       <tr>
-        <td>カーソルのある行をコピーする。</td>
+        <td>カーソルのある行をコピーする</td>
         <td><kbd>Ctrl + C</kbd></td>
         <td><kbd>&#8984; + C</kbd></td>
       </tr>
       <tr>
-        <td>カーソルのある行をカットする。</td>
+        <td>カーソルのある行をカットする</td>
         <td><kbd>Ctrl + X</kbd></td>
         <td><kbd>&#8984; + X</kbd></td>
       </tr>
       <tr>
-        <td>カーソルのある行を上・下に移動する。</td>
+        <td>カーソルのある行を上・下に移動する</td>
         <td><kbd>Alt + ↑・↓</kbd></td>
         <td><kbd>&#8997; + ↑・↓</kbd></td>
       </tr>
       <tr>
-        <td>元に戻す（undo）。</td>
+        <td>元に戻す（undo）</td>
         <td><kbd>Ctrl + Z</kbd></td>
         <td><kbd>&#8984; + Z</kbd>
         </td>
       </tr>
       <tr>
-        <td>やり直す（redo）。</td>
+        <td>やり直す（redo）</td>
         <td><kbd>Ctrl + Y</kbd></td>
         <td><kbd>&#8984; + &#8679; + Z</kbd></td>
       </tr>
       <tr>
-        <td>コメントアウトをオン・オフにする。</td>
+        <td>コメントアウトをオン・オフにする</td>
         <td><kbd>Ctrl + /</kbd></td>
         <td><kbd>&#8984; + /</kbd></td>
       </tr>
       <tr>
-        <td>インデントを整える。</td>
+        <td>インデントを整える</td>
         <td><kbd>Shift + Alt + F</kbd></td>
         <td><kbd>&#8679; + &#8997; + F</kbd></td>
       </tr>
@@ -128,12 +125,12 @@
     </thead>
     <tbody>
       <tr>
-        <td>右のタブに移動する。</td>
+        <td>右のタブに移動する</td>
         <td><kbd>Ctrl + Tab</kbd></td>
         <td><kbd>&#8963; + tab</kbd></td>
       </tr>
       <tr>
-        <td>左・右のタブに移動する。</td>
+        <td>左・右のタブに移動する</td>
         <td></td>
         <td><kbd>&#8984; + &#8997; + ←・→</kbd></td>
       </tr>
@@ -152,27 +149,27 @@
     </thead>
     <tbody>
       <tr>
-        <td>カーソルをファイルの先頭・末尾に移動する。</td>
+        <td>カーソルをファイルの先頭・末尾に移動する</td>
         <td><kbd>Ctrl + Home・End</kbd></td>
         <td><kbd>&#8984; + ↑・↓</kbd></td>
       </tr>
       <tr>
-        <td>カーソルを単語毎に移動する。</td>
+        <td>カーソルを単語毎に移動する</td>
         <td><kbd>Ctrl + ←・→</kbd></td>
         <td><kbd>&#8997; + ←・→</kbd></td>
       </tr>
       <tr>
-        <td>カーソルを指定した行に移動する。</td>
+        <td>カーソルを指定した行に移動する</td>
         <td><kbd>Ctrl + G</kbd></td>
         <td><kbd>&#8963; + G</kbd></td>
       </tr>
       <tr>
-        <td>カーソルを上・下に挿入する。</td>
+        <td>カーソルを上・下に挿入する</td>
         <td><kbd>Ctrl + Alt + ↑・↓</kbd></td>
         <td><kbd>&#8984; + &#8997; + ↑・↓</kbd></td>
       </tr>
       <tr>
-        <td>カーソル位置にある単語と同じ単語を1つずつ範囲範囲に追加する。</td>
+        <td>カーソル位置にある単語と同じ単語を1つずつ範囲範囲に追加する</td>
         <td><kbd>Ctrl + Shift + L</kbd></td>
         <td><kbd>&#8984; + D</kbd></td>
       </tr>
@@ -191,12 +188,12 @@
     </thead>
     <tbody>
       <tr>
-        <td>ファイル内を検索する。</td>
+        <td>ファイル内を検索する</td>
         <td><kbd>Ctrl + F</kbd></td>
         <td><kbd>&#8984; + F</kbd></td>
       </tr>
       <tr>
-        <td>複数のファイルから検索する。</td>
+        <td>複数のファイルから検索する</td>
         <td><kbd>Ctrl + Shift + F</kbd></td>
         <td><kbd>&#8984; + &#8679; + F</kbd></td>
       </tr>
@@ -215,12 +212,12 @@
     </thead>
     <tbody>
       <tr>
-        <td>選択範囲を昇順に並び替える。</td>
+        <td>選択範囲を昇順に並び替える</td>
         <td><kbd>Ctrl + Shift + P</kbd> <code>Sort Lines Ascending</code></td>
         <td><kbd>&#8984; + &#8679; + P</kbd> <code>Sort Lines Ascending</code></td>
       </tr>
       <tr>
-        <td>選択範囲を降順に並び替える。</td>
+        <td>選択範囲を降順に並び替える</td>
         <td><kbd>Ctrl + Shift + P</kbd> <code>Sort Lines Descending</code></td>
         <td><kbd>&#8984; + &#8679; + P</kbd> <code>Sort Lines Descending</code></td>
       </tr>
@@ -228,10 +225,8 @@
   </table>
 </div><!-- /.table-responsive -->
 <h2 id="emmet" class="title">Emmet</h2>
-<p>HTML5の雛形を入力する。</p>
-<ul>
-  <li><code>html:5</code>と入力し、<kbd>tab</kbd>を押す、または、<kbd>! tab</kbd>を押すと、以下が自動的に入力される。</li>
-</ul>
+<h3 class="oper">HTML5の雛形を入力する</h3>
+<p><code>html:5</code>と入力し、<kbd>tab</kbd>を押す、または、<kbd>! tab</kbd>を押すと、以下が自動的に入力される。</p>
 <pre class="block"><code>&lt;!DOCTYPE html>
 &lt;html lang="ja">
 &lt;head>
@@ -243,10 +238,8 @@
 
 &lt;/body>
 &lt;/html></code></pre>
-<p>リストを入力する。</p>
-<ul>
-  <li><kbd>ul&gt;li*5</kbd>と入力し、<kbd>Enter</kbd>を押すと、以下が自動的に入力される。</li>
-</ul>
+<h3 class="oper">リストを入力する</h3>
+<p><code>ul&gt;li*5</code>と入力し、<kbd>Enter</kbd>を押すと、以下が自動的に入力される。</p>
 <pre class="block"><code>&lt;ul>
   &lt;li>&lt;/li>
   &lt;li>&lt;/li>
@@ -254,23 +247,4 @@
   &lt;li>&lt;/li>
   &lt;li>&lt;/li>
 &lt;/ul></code></pre>
-<h2 id="browser" class="title">open in browser</h2>
-<div class="table-responsive">
-  <table class="table table-striped table-sm">
-    <thead>
-      <tr>
-        <th>操作</th>
-        <th>Windowsの場合</th>
-        <th>macOSの場合</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>ファイルをWebブラウザで開く。</td>
-        <td><kbd>Alt + B</kbd></td>
-        <td><kbd>&#8997; + B</kbd></td>
-      </tr>
-    </tbody>
-  </table>
-</div><!-- /.table-responsive -->
 @endsection
--- resources/views/article/tool/vscode/setting.blade.php
+++ resources/views/article/tool/vscode/setting.blade.php
@@ -3,12 +3,22 @@
 @section('contents')
 <header>
 @include('components.header')
+  <nav>
+    <ul class="toc">
+      <li>
+        <a href="#basic">基本設定</a>
+      </li>
+      <li>
+        <a href="#programming">プログラミング言語固有の設定</a>
+      </li>
+    </ul>
+  </nav>
 </header>
-<h2 class="title">基本設定</h2>
+<h2 id="basic" class="title">基本設定</h2>
 <p>1. コマンドパレットを開く。</p>
 <p>2. <code>settings.json</code>と入力する。</p>
 <p>3. <code>基本設定: 設定 (JSON) を開く</code>を選択し、<kbd>Enter</kbd>を押す。</p>
-<h2 class="title">プログラミング言語固有の設定</h2>
+<h2 id="programming" class="title">プログラミング言語固有の設定</h2>
 <p>1. コマンドパレットを開く。</p>
 <p>2. <code>Preferences: Configure Language Specific Settings</code>と入力する。</p>
 <p>3. <code>基本設定: 言語固有の設定を構成します</code>を選択し、<kbd>Enter</kbd>を押す。</p>
