ba27aa2d342b460ef36dc5b39e57c44ae1745364
Hiro <hiroyuki1126@gmail.com>
2021-07-02 22:39:46 +0900

    url関数をsecure_url関数に変更。

--- resources/views/sitemap/sitemap.blade.php
+++ resources/views/sitemap/sitemap.blade.php
@@ -3,14 +3,14 @@
 @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>{{ url('/') }}</loc>
+    <loc>{{ secure_url('/') }}</loc>
     <lastmod>{{ date('Y-m-01') }}</lastmod>
   </url>
 @foreach (config('article.ARTICLE_TITLES') as $_category_id => $_category_info)
   @foreach ($_category_info as $_subcategory_id => $_subcategory_info)
     @foreach ($_subcategory_info as $_article_id => $_article_info)
   <url>
-    <loc>{{ url($_article_info[0]) }}</loc>
+    <loc>{{ secure_url($_article_info[0]) }}</loc>
     <lastmod>{{ $_article_info[3] }}</lastmod>
   </url>
     @endforeach
